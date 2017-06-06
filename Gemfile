52b637a552387d519c68cf6faa420e87e68523f0
Meagan Waller <meaganewaller@gmail.com>
2017-06-06 13:49:23 -0400

    Big refactor, starts applying bootstrap type theme

--- Gemfile
+++ Gemfile
@@ -2,8 +2,12 @@ source 'https://rubygems.org'
 
 gem 'middleman',      '~> 4.2'
 gem 'middleman-blog', '~> 4.0'
-
-gem 'slim'
+gem 'middleman-minify-html'
+gem 'haml'
+gem 'middleman-pry'
+gem 'nokogiri'
+gem 'haml_lint'
+gem 'scss_lint'
 
 group :test, :development do
   gem 'rspec'
--- config.rb
+++ config.rb
@@ -1,30 +1,30 @@
-require 'slim'
-
-page '/*.xml',  layout: false
-page '/*.json', layout: false
-page '/*.txt',  layout: false
-
-activate :external_pipeline,
-  name: :gulp,
-  command: build? ? 'gulp build --production' : './node_modules/gulp/bin/gulp.js',
-  source: "dist",
-  latency: 1
-
-activate :blog do |blog|
-  blog.prefix = "blog"
-end
-
 activate :directory_indexes
 
-configure :build do
-  ignore 'assets/*'
-  activate :gzip
-  activate :asset_hash
-  activate :relative_assets
+set :relative_links, true
+set :haml, { format: :html5 }
 
-  helpers do
-    def markdown(text)
-      Tilt['markdown'].new { text }.render(scope=self)
-    end
+page '/*.xml',    layout: false
+page '/*.json',   layout: false
+page '/*.txt',    layout: false
+page '/404.html', directory_index: false
+
+set :css_dir,    "assets/stylesheets"
+set :images_dir, "assets/images"
+set :js_dir,     "assets/javascripts"
+
+activate :external_pipeline,
+  name:    :gulp,
+  command: "./node_modules/gulp/bin/gulp.js",
+  source:  ".tmp",
+  latency: 1
+
+configure :build do
+  ignore "assets/javascripts/app.js"
+  ignore "assets/stylesheets/site"
+
+  activate :gzip
+  activate :minify_html do |html|
+    html.remove_quotes = false
+    html.remove_intertag_spaces = true
   end
 end
